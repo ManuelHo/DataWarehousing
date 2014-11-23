7f93a4130ebc73c4321634128ffb27105190aae0
ManuelHo <manuel.hochreiter@atn.ac.at>
2014-11-23 18:26:52 +0100

    blabla

--- DWExercise2/Index.sql
+++ DWExercise2/Index.sql
@@ -1,29 +1,29 @@
-CREATE INDEX IDX_SALE_CUSTOMERID ON Sale (customerId) NOPARALLEL;
+CREATE INDEX IDX_SALE_CUSTOMERID ON Sales (customerId) NOPARALLEL;
 
-CREATE INDEX IDX_SALE_PAYMENTID ON Sale (paymentId) NOPARALLEL;
+CREATE INDEX IDX_SALE_PAYMENTID ON Sales (paymentId) NOPARALLEL;
 
-CREATE INDEX IDX_SALE_PRODUCTEAN ON Sale (productEan) NOPARALLEL;
+CREATE INDEX IDX_SALE_PRODUCTEAN ON Sales (productEan) NOPARALLEL;
 
-CREATE INDEX IDX_SALE_SHIPDATE ON Sale (shipDate) NOPARALLEL;
+CREATE INDEX IDX_SALE_SHIPDATE ON Sales (shipDate) NOPARALLEL;
 
-CREATE INDEX IDX_SALE_ORDERDATE ON Sale (orderDate) NOPARALLEL;
+CREATE INDEX IDX_SALE_ORDERDATE ON Sales (orderDate) NOPARALLEL;
 
-CREATE BITMAP INDEX IDX_SALE_MODEOFSHIPMENT ON Sale (modeOfShipment) NOPARALLEL;
+CREATE BITMAP INDEX IDX_SALE_MODEOFSHIPMENT ON Sales (modeOfShipment) NOPARALLEL;
 
 -- BITMAP JOIN INDEXES:
 
-CREATE BITMAP INDEX IDX_PRODUCT_SALE ON Sale(prodName, prodDescription, categoryId, catDescription)
-	FROM Sale s, Product p
+CREATE BITMAP INDEX IDX_PRODUCT_SALE ON Sales(prodName, prodDescription, categoryId, catDescription)
+	FROM Sales s, Product p
 	WHERE s.productEan = p.productEan;
 
-CREATE BITMAP INDEX IDX_CUST_SALE ON Sale (address, corpName, givenName, familyName)
-	FROM Sale s, Product p
-	WHERE s.customerId = p.customerId;
+CREATE BITMAP INDEX IDX_CUST_SALE ON Sales (address, corpName, givenName, familyName)
+	FROM Sales s, Customer c
+	WHERE s.customerId = c.customerId;
 
-CREATE BITMAP INDEX IDX_ORDER_SALE ON Sale (nDay, nMonth, nQuarter , nYear)
-	FROM Sale s, DateTable d 
+CREATE BITMAP INDEX IDX_ORDER_SALE ON Sales (nDay, nMonth, nQuarter , nYear)
+	FROM Sales s, DateTable d 
 	WHERE s.shipDate = d.dateId;
 
-REATE BITMAP INDEX IDX_PAYMENT_SALE ON Sale(pmType, actOwner, ccOwner)
-	FROM Sale s, DateTable d
-	WHERE s.paymentId = d.paymentId;
+CREATE BITMAP INDEX IDX_PAYMENT_SALE ON Sales(pmType, actOwner, ccOwner)
+	FROM Sales s, Payment p
+	WHERE s.paymentId = p.paymentId;
